aff83eae9e4348c61042cf7c2099d10cafd2d9c3
Alan Vivona <alan.g.vivona@gmail.com>
2018-03-27 13:52:45 -0300

    Few random changes

--- app/src/api/app/app.go
+++ app/src/api/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"api/app/items"
 	"database/sql"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,11 +25,17 @@ func StartApp() {
 }
 
 func configDataBase() *sql.DB {
-	// db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "user", "userpwd", "db", "db"))
-	//if err != nil {
-	//	panic("Could not connect to the db")
-	//} else {
-	//	return db
-	//}
-	return nil
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", "user", "userpwd", "db", "3306", "db"))
+
+	fmt.Println("HERE ======")
+	fmt.Println("connection STRING = ", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", "user", "userpwd", "db", "3306", "db"))
+	fmt.Println("err = ", err)
+	fmt.Println("THERE ======")
+
+	if err != nil {
+		panic("Could not connect to the db")
+	} else {
+		return db
+	}
 }
