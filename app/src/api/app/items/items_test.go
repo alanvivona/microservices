d178741e1fa7f653cf6135999a96e69835633535
Alan Vivona <alan.vivona@enlightedinc.com>
2018-04-01 11:00:00 -0300

    Unit tests for items controller skeleton

--- app/src/api/app/items/items_test.go
+++ app/src/api/app/items/items_test.go
@@ -11,29 +11,154 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func TestHandler(t *testing.T) {
+func ConfigureRouter() *Engine {
 	router := gin.Default()
 	Configure(router, nil)
+	return router
+}
+
+func TestGetItem(t *testing.T) {
+	router := ConfigureRouter()
 
 	// Inject our mock into our handler.
 	var is mock.ItemService
 	Is = &is
 
-	// Mock our User() call.
-	is.ItemFn = func(id string) (*models.Item, error) {
-		if id != "100" {
-			t.Fatalf("unexpected id: %d", id)
-		}
-		return &models.Item{ID: "100", Name: "DaItam", Description: "Elnesto"}, nil
+	is.ItemFn = func(id int) (*models.Item, error) {
+		return &models.Item{ID: id, Name: "Test result name", Description: "Test result description"}, nil
 	}
 
-	// Invoke the handler.
-	w := httptest.NewRecorder()
-	r, _ := http.NewRequest("GET", "/item/100", nil)
-	router.ServeHTTP(w, r)
+	testCases := []struct {
+		method                string
+		url                   string
+		shouldServiceBeCalled bool
+	}{
+		{"GET", "/item/1", true},
+		{"GET", "/item/-1", false},
+		{"GET", "/item/0", false},
+		{"GET", "/item/000001", true},
+		{"GET", "/item/99999999999999999999", false},
+		{"GET", "/item/a", false},
+	}
 
-	// Validate mock.
-	if !is.ItemInvoked {
-		t.Fatal("expected User() to be invoked")
+	for _, testCase := range testCases {
+		is.ItemInvoked = false
+		// Invoke the handler.
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(testCase.method, testCase.url, nil)
+		router.ServeHTTP(w, r)
+		if is.ItemInvoked != testCase.shouldServiceBeCalled {
+			t.Fatal("expected GetItem() to be invoked")
+		}
+	}
+}
+
+func TestGetItems(t *testing.T) {
+	router := ConfigureRouter()
+
+	// Inject our mock into our handler.
+	var is mock.ItemService
+	Is = &is
+
+	is.ItemsFn = func() ([]*models.Item, error) {
+		// TODO: return list of items
+		return nil, nil
+	}
+
+	testCases := []struct {
+		method                string
+		url                   string
+		shouldServiceBeCalled bool
+	}{
+		{"GET", "/item/", true},
+		{"GET", "/item/0", false},
+	}
+
+	for _, testCase := range testCases {
+		is.ItemsInvoked = false
+		// Invoke the handler.
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(testCase.method, testCase.url, nil)
+		router.ServeHTTP(w, r)
+		if is.ItemsInvoked != testCase.shouldServiceBeCalled {
+			t.Fatal("expected GetItems() to be invoked")
+		}
+	}
+}
+
+func TestPostItem(t *testing.T) {
+	router := ConfigureRouter()
+
+	// Inject our mock into our handler.
+	var is mock.ItemService
+	Is = &is
+
+	is.CreateItemFn = func() ([]*models.Item, error) {
+		// TODO: return item data
+		return nil, nil
+	}
+
+	testCases := []struct {
+		method                string
+		url                   string
+		body                  string
+		shouldServiceBeCalled bool
+	}{
+		{"POST", "/item/", "{", false},
+		{"POST", "/item/", "{'fakeproperty':'value'}", false},
+		{"POST", "/item/", "{'name':'test name'}", false},
+		{"POST", "/item/", "{'name':'test name', 'fakeproperty': 'value'}", false},
+		{"POST", "/item/", "{'name':'test name', 'description': 'test desc'}", true},
+		{"POST", "/item/", "{'name':'test name ---- really long name that does not meet the criteria', 'description': 'test desc'}", false},
+		{"POST", "/item/", "{'name':'test name', 'description': 'test desc ---- really long desc that does not meet the criteria'}", false},
+	}
+
+	for _, testCase := range testCases {
+		is.CreateItemInvoked = false
+		// Invoke the handler.
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(testCase.method, testCase.url, testCase.body)
+		router.ServeHTTP(w, r)
+		if is.CreateItemInvoked != testCase.shouldServiceBeCalled {
+			t.Fatal("expected PostItem() to be invoked")
+		}
+	}
+}
+
+func TestDeleteItem(t *testing.T) {
+	router := ConfigureRouter()
+
+	// Inject our mock into our handler.
+	var is mock.ItemService
+	Is = &is
+
+	is.DeleteItemFn = func() ([]*models.Item, error) {
+		// TODO: return item data
+		return nil, nil
+	}
+
+	testCases := []struct {
+		method                string
+		url                   string
+		shouldServiceBeCalled bool
+	}{
+		{"DELETE", "/item/1", true},
+		{"DELETE", "/item/-1", false},
+		{"DELETE", "/item/0", false},
+		{"DELETE", "/item/999999999999999", false},
+		{"DELETE", "/item/001", true},
+		{"DELETE", "/item/a", false},
+		{"DELETE", "/item/", false},
+	}
+
+	for _, testCase := range testCases {
+		is.DeleteItemInvoked = false
+		// Invoke the handler.
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(testCase.method, testCase.url, nil)
+		router.ServeHTTP(w, r)
+		if is.DeleteItemInvoked != testCase.shouldServiceBeCalled {
+			t.Fatal("expected DeleteItem() to be invoked")
+		}
 	}
 }
--- app/src/api/app/mock/mock.go
+++ app/src/api/app/mock/mock.go
@@ -4,7 +4,7 @@ import "api/app/models"
 
 // ItemService ...
 type ItemService struct {
-	ItemFn      func(id string) (*models.Item, error)
+	ItemFn      func(id int) (*models.Item, error)
 	ItemInvoked bool
 
 	ItemsFn      func() ([]*models.Item, error)
@@ -18,7 +18,7 @@ type ItemService struct {
 }
 
 // Item ...
-func (is *ItemService) Item(id string) (*models.Item, error) {
+func (is *ItemService) Item(id int) (*models.Item, error) {
 	is.ItemInvoked = true
 	return is.ItemFn(id)
 }
@@ -36,7 +36,7 @@ func (is *ItemService) CreateItem(i *models.Item) error {
 }
 
 // DeleteItem ...
-func (is *ItemService) DeleteItem(id string) error {
+func (is *ItemService) DeleteItem(id int) error {
 	is.DeleteItemInvoked = true
 	return is.DeleteItemFn(id)
 }
